a098b816c3e749c57db2e794700a50ca670123e5
J. P. Visser <jpvisser827@tutanota.com>
2022-08-01 20:26:09 +0200

    Re-add Message.java

---
+++ client/src/Message.java
@@ -0,0 +1,12 @@
+import java.io.Serializable;
+
+public class Message implements Serializable {
+    String sender, receiver, content;
+
+    public Message(String sender, String receiver, String content)
+    {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.content = content;
+    }
+}
